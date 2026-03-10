0200cb5ae73a36a318347a2b5fb1194a8ca6e21b
Jean De Jesus <39771322+AinzuOnDaGit@users.noreply.github.com>
2026-03-10 16:33:15 +0000

    saved

--- a3.py
+++ a3.py
@@ -1,17 +1,17 @@
-def log1(num=3):
-    
+def log1(num=3): 
     while num != 0:
-        user1 = input("Enter username: ")
-        if user1 == "jd824":
+
+        user2 = input("Enter Password: ")
+        if user2 == "jd824":
             print("Granted")
             break
         else:
             num -= 1 
-            print(f"Nuh uh {num} Chance")
+            print(f"Wrong Password {num} Tries left")
 
             print("Try again") if num else print("Too many wrong attempts. You are locked out!")
-            
-            
+
+
 def main_menu():
     print("Welcome please enter choice: ")
     print("1. Login simulation")
