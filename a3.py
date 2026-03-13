aa3754f85d0a1db5dd038b67f2a1530c168578e4
Jean De Jesus <39771322+AinzuOnDaGit@users.noreply.github.com>
2026-03-13 16:37:36 +0000

    python

--- a3.py
+++ a3.py
@@ -6,10 +6,18 @@ import fleep #Using a detection of PDF
 import os
 
 def asgn1():
-    file_input = input("Enter the name of the file, with the extention. Example: file1.pdf: ").strip()
-    path = "/workspaces/advos1"
-    full_path = os.path.join(path, file_input)
-    shutil.move(full_path, "/workspaces/advos1/assignments")
+    file_input = input("Enter the name of the file, with the extension. Example: file1.pdf: ") #Finding Document on file
+    path = "/workspaces/advos1" #Created file existing
+    full_path = os.path.join(path, file_input) 
+    
+    #If file exist, will be sent on the folder
+    if os.path.isfile(full_path):
+        shutil.move(full_path, "/workspaces/advos1/assignments") #Moving the existed file into a folder
+    
+    #If file doesn't exist, Message invalid choice
+    else: 
+        print("Invalid, Try again")
+
 
 def asgn2(): #Check all submission
     path1 = "/workspaces/advos1/assignments" 
@@ -17,6 +25,42 @@ def asgn2(): #Check all submission
     print("Files and directories in '", path1, "' :")
     # prints all files
     print(dir_list)	
+
+
+def file_detect(): #Detection file
+    fdcheck = input("Submit a file here: File must be .PDF or .DOCX: ")
+    fdpath = "/workspaces/advos1"
+    fd_join = os.path.join(fdpath, fdcheck) 
+    ex_path = os.path.splitext(fd_join)[1].lower()
+    
+    #If file doesn't exist on folder
+    if not os.path.isfile(fd_join):
+        print("FILE DOES NOT EXIST, TRY AGAIN")     
+        return
+    
+
+    #Detection of file size (Must be lower than 5MB)
+    if os.path.isfile(fd_join):
+        fd_size = os.path.getsize(fd_join)
+
+        if fd_size > 5000000:
+            print("FILE SIZE LARGE, Must be lower than 5MB")
+            return
+    
+
+
+    # DETECTION IF FILE IS PDF OR DOCX 
+    if ex_path == ".pdf": 
+        print("FILE DETECTED: .PDF")
+        shutil.move(fd_join, "/workspaces/advos1/assignments")
+        return
+    elif ex_path == ".docx": 
+        print("FILE DETECTED: .DOCX")
+        shutil.move(fd_join, "/workspaces/advos1/assignments")
+        return
+    else:
+        print("FILE DETECTED NOT PDF OR DOCX, TRY AGAIN!!!")
+
 
 
 
@@ -29,7 +73,7 @@ def log1(num=3):
     while num != 0:
 
         user2 = input("Enter Password: ")
-        if user2 == "jd824":
+        if user2 == "p455w0rd1!":
             print("Granted")
             logging.basicConfig(format='%(asctime)s %(message)s')
             logging.warning("Login Attempt success")
@@ -45,6 +89,7 @@ def log1(num=3):
             logging.warning("Login Attempt Failed")
             sys.exit
 
+#Exit Function
 def exmd1():
     ex1 = input("Do you wish to exit? Press Y or N: ").lower()
     if ex1 == "y":
@@ -57,6 +102,7 @@ def exmd1():
     else:
         print("Please enter Y or N")
         exmd1()
+
 
 def main_menu():
     print("Welcome please enter choice: ")
@@ -75,7 +121,7 @@ def main_menu():
             asgn2()
             break
         elif model == "3":
-            
+            file_detect()
             break
         elif model == "4": 
             log1()
